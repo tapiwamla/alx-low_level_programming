8068d6a8b2bc59bab8c41a9d171a833381926c11
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-15 14:51:23 -0700

    What I do best

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function, application entry point
+ * Return: 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
