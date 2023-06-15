cb86499c06355836d8bd17c1e77d2821c9e5b6dd
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-15 11:12:21 -0700

    Printf, size, 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,9 @@
 int main(void)
 {
 printf("Size of a char: %i byte(s)\n", sizeof(char));
-printf("Size of a short: %i byte(s)\n", sizeof(short));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("Size of a long: %i byte(s)\n", sizeof(long));
-printf("Size of a long long: %i byte(s)\n", sizeof(long long));
+printf("Size of a long: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i byte(s)\n", sizeof(float));
-printf("Size of a double: %i byte(s)\n", sizeof(double));
-printf("Size of a long double: %i byte(s)\n", sizeof(long double));
 return (0);
 }
