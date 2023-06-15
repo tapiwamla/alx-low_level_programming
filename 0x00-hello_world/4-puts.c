22fed0fde3882ba97687f298c3707f458d84a44e
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-15 09:45:47 -0700

    Not printf, but puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/* Include the standard input-output */
+#include <stdio.h>
+
+/* Program's entry point */
+int main(void)
+{
+  puts("\"Programming is like building a mulilingual puzzle");
+  return (0);
+}
