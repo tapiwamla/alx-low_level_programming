d910cf2d2d978fc7955fde4282df11f8e764f709
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-16 13:31:34 -0700

    Block location

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,14 @@
 int main(void)
 {
 	int n;
-	if(n<0)
-	    printf("%d is negative\n", n);
-	else if(n==0)
-	  printf("%d is zero\n", n);
-	else
-	    printf("%d is positive\n", n);
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	if(n<0)
+            printf("%d is negative\n", n);
+        else if(n==0)
+            printf("%d is zero\n", n);
+        else
+            printf("%d is positive\n", n);
 	/* your code goes there */
 	return (0);
 }
