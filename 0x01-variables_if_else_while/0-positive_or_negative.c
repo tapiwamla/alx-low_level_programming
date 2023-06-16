5e205b2fade97325298b0d4e830e4e14b045c96b
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-16 14:13:03 -0700

    Betty

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+  * main - entry point of application
+  * Return: (0), success
+  */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n < 0)
+{
+printf ("%d is negative\n", n);
+}
+else if(n == 0)
+{
+printf ("%d is zero\n", n);
+}
+else
+{
+printf ("%d is positive\n", n);
+}
+/* your code goes there */
+return (0);
+}
