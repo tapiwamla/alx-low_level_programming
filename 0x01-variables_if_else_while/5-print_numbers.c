3aafbb26b09a2109a5e39eed4b1e201bedd7d4af
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-16 16:22:33 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - program entry point
+  * Return: 0 (success)
+  * Description: Print 0-9
+  */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
