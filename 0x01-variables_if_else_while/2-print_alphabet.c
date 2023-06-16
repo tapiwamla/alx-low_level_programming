a854108a439441494b38de73f0a5d580704e9b6d
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-16 15:30:12 -0700

    Betty, again

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,13 @@
   * Return: 0 (success)
   * Description: A program that prints all lowercase letters of the alphabet
   */
-int main() {
+int main(void)
+{
 char character;
 
-for (character = 'a'; character <= 'z'; character++) {
-    putchar(character);
+for (character = 'a'; character <= 'z'; character++)
+{
+putchar(character);
 }
 putchar('\n');
 return (0);
