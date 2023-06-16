64b184f83cf70feef194f9e1db9d1d5c7303e383
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-16 15:37:23 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - program entry point
+  * Return: 0 (success)
+  * Description: A program that prints all letters of the alphabet
+  */
+int main()
+{
+char lower;
+char upper;
+
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+putchar(lower);
+}
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+putchar(upper);
+}
+putchar('\n');
+return (0);
+}
