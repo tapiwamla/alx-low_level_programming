702c4aff3f5b2fde067603147b67c0c97973a111
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-16 13:22:26 -0700

    Fixed, new line, braces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,11 @@ int main(void)
 {
 	int n;
 	if(n<0)
-	  {
-	    printf("%d is negative", n);
-	  }
+	    printf("%d is negative\n", n);
+	else if(n==0)
+	  printf("%d is zero\n", n);
 	else
-	  {
-	    printf("%d is positive", n);
-	  }
+	    printf("%d is positive\n", n);
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
