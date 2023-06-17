bacfe7c6a9bbf99141a9a8a03255c914cc4b8dd8
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-16 17:54:34 -0700

    Combinations+++

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: 0 (success)
+ */
+int main() {
+int first, second;
+for (first = 0; first <= 99; first++) {
+for (second = first + 1; second <= 99; second++) {
+putchar(first / 10 + '0');
+putchar(first % 10 + '0');
+putchar(' ');
+putchar(second / 10 + '0');
+putchar(second % 10 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
