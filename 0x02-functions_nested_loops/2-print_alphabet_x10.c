119807cf32186699b97ff7ffe186009b525f827e
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-20 13:59:18 -0700

    Ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lowercase alphabet x10
+ * return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+char character = 'a';
+int count = 0;
+
+while(count < 10)
+{
+while (character <= 'z')
+{
+_putchar(character);
+character++;
+}
+_putchar('\n');
+}
+}
