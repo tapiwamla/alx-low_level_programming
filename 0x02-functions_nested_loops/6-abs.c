2389f19e5cb93751d3e1f96c77fef28bc09a3448
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-20 16:15:24 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - Computes the absolute value
+ * @n: The integer for which absolute value is to be calculated
+ * Return: The absolute value of the input integer
+ */
+int _abs(int n)
+{
+if (n < 0)
+return -n;
+else
+return n;
+}
