c078d0c41edaf8912b6fd2f1abb5ca23bf47c25a
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-20 13:35:33 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * alphabet - print lowercase alphabet
+ * print_alphabet - print lowercase alphabet
  * return: 0 (success)
  */
 void print_alphabet(void)
 {
-char character = 0;
+char character = 'a';
 while (character < 'z')
 {
 _putchar(character);
