73cda90e832c498ca95cfd49681e0f56f6482e17
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-21 09:29:15 -0700

    Natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return: 0 (success)
+ */
+int main(void) {
+int sum = 0;
+
+for (int i = 1; i < 1024; i++) {
+if (i % 3 == 0 || i % 5 == 0) {
+sum += i;
+}
+}
+
+printf("%d\n", sum);
+return (0);
+}
