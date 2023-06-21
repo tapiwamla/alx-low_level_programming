ef6c62b2b98e9fc7b70394fa6fc48c7077588ed5
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-21 14:29:43 -0700

    Reduced lines

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,59 +1,43 @@
-#include "main.h"
-
+nclude "main.h"
 /**
-* print_times_table - Prints the multiplication table from 0 to n
-* @n: The number up to which the multiplication table is printed
-*/
+ * * print_times_table - Prints the n times table
+ * * @n: The number to generate the times table for
+ * */
 void print_times_table(int n)
 {
+	int row;
+	int column;
 
-int row;
-int column;
-int product;
-if (n > 15 || n < 0)
-return;
+	if (n > 15 || n < 0)
+		return;
+	for (row = 0; row <= n; i++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			int product = row * column;
 
-for (row = 0; row <= n; row++)
-{
-for (column = 0; column <= n; column++)
-{
-product = row * column;
+			if (column != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				if (product < 100)
+					_putchar(' ');
+			}
 
-if (product < 10)
-{
-if (column != 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
-}
-else if (product < 100)
-{
-_putchar(' ');
-}
+			if (product >= 100)
+			{
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+			}
 
-if (product < 10)
-{
-_putchar(product + '0');
-}
-else if (product < 100)
-{
-_putchar((product / 10) + '0');
-_putchar((product % 10) + '0');
-}
-else
-{
-_putchar((product / 100) + '0');
-_putchar(((product / 10) % 10) + '0');
-_putchar((product % 10) + '0');
-}
-
-if (column != n)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
