6077d6a0f018ceb9675c5684da99f21ec5214bcb
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-21 09:02:54 -0700

    Betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,59 +1,59 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the multiplication table from 0 to n
- * @n: The number up to which the multiplication table is printed
- */
+* print_times_table - Prints the multiplication table from 0 to n
+* @n: The number up to which the multiplication table is printed
+*/
 void print_times_table(int n)
 {
-    int row;
-    int column;
-    int product;
 
-    if (n > 15 || n < 0)
-        return;
+int row;
+int column;
+int product;
+if (n > 15 || n < 0)
+return;
 
-    for (row = 0; row <= n; row++)
-    {
-        for (column = 0; column <= n; column++)
-        {
-            product = row * column;
-            
-            if (product < 10)
-            {
-                if (column != 0)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-            }
-            else if (product < 100)
-            {
-                _putchar(' ');
-            }
-            
-            if (product < 10)
-            {
-                _putchar(product + '0');
-            }
-            else if (product < 100)
-            {
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-            else
-            {
-                _putchar((product / 100) + '0');
-                _putchar(((product / 10) % 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-            
-            if (column != n)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+product = row * column;
+
+if (product < 10)
+{
+if (column != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+}
+else if (product < 100)
+{
+_putchar(' ');
+}
+
+if (product < 10)
+{
+_putchar(product + '0');
+}
+else if (product < 100)
+{
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+else
+{
+_putchar((product / 100) + '0');
+_putchar(((product / 10) % 10) + '0');
+_putchar((product % 10) + '0');
+}
+
+if (column != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
