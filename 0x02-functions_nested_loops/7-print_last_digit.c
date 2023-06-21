db75951be8458b02ab91f103cce15c21b4e79364
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-20 17:12:21 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to be evaluated
+ * Return:  0 (success)
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
