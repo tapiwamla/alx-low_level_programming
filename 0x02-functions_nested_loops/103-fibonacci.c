57b6ea85b63e768e8c964667be1096a6411eb33c
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-21 15:27:43 -0700

    Fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 /**
- * find_even_fibonacci_sum - Finds the sum of even-valued terms in the Fibonacci sequence.
+ * find_even_fibonacci_sum - Find sum of evens
  * @limit: The upper limit for Fibonacci numbers.
  * main - entry point of the program.
  * Return: The sum of even-valued Fibonacci numbers.
  */
 unsigned long find_even_fibonacci_sum(unsigned long limit);
+/**
+ * main - entry point of the program
+ */
 int main(void)
 {
 unsigned long limit = 4000000;
 unsigned long sum = find_even_fibonacci_sum(limit);
-printf("Sum of even-valued Fibonacci terms below %lu: %lu\n", limit, sum);
+printf(sum);
 return (0);
 }
 /**
- * find_even_fibonacci_sum - Finds the sum of even-valued terms in the Fibonacci sequence.
+ * find_even_fibonacci_sum - Find sum of evens
  * @limit: The upper limit for Fibonacci numbers.
  * Return: The sum of even-valued Fibonacci numbers.
  */
