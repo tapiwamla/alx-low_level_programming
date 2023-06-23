02efee4813d4255a16d299f45d73f649ba1240c6
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-23 13:37:40 -0700

    Braces

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@ void print_most_numbers(void)
 char num = '0';
 
 while (num <= '9')
-if (num != 2 && num != 4)
 {
+if (num != 2 && num != 4)
 {
 _putchar(num);
 num++;
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
