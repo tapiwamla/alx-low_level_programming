a33165d3bb86907e84e063b206fbf8ddf3254049
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-23 13:34:20 -0700

    Most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_numbers - prints 0-9 except 2,4
+* Return: null
+*/
+void print_most_numbers(void)
+{
+char num = '0';
+
+while (num <= '9')
+if (num != 2 && num != 4)
+{
+{
+_putchar(num);
+num++;
+}
+_putchar('\n');
+}
+}
