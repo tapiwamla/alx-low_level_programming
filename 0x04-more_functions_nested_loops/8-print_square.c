1bf42068d9df96755c3da67ea6031ad62e73b0f1
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-23 14:44:29 -0700

    Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *print_square - draw a square on terminal
 *@n: number
@@ -6,14 +7,18 @@
 *Return: (void)
 */
 
-void print_square(int size) {
-if (size <= 0) {
+void print_square(int size)
+{
+if (size <= 0)
+{
 _putchar('\n');
 return;
 }
 
-for (int row = 0; row < size; row++) {
-for (int col = 0; col < size; col++) {
+for (int row = 0; row < size; row++)
+{
+for (int col = 0; col < size; col++)
+{
 _putchar('#');
 }
 _putchar('\n');
