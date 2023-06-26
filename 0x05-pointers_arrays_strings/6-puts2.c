b93c8e156b2d33e8df514070d0dcb8833eba4846
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-26 14:46:15 -0700

    Puts, again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,20 +2,21 @@
 /**
  * puts2 - Prints every other character of a string
  * @str: The string to be printed
- *
  * Return: None
  */
 void puts2(char *str)
 {
 int i = 0;
+int count = 0;
 
-if (str != NULL)
+while (str[count] != '\0')
 {
-while (str[i] != '\0')
+count++;
+}
+while (i <= count - 1)
 {
 _putchar(str[i]);
 i += 2;
-}
 }
 _putchar('\n');
 }
