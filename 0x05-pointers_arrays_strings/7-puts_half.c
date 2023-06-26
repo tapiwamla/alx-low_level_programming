a938f6b69a89e0d4967743f709ff147d5cb0ed9c
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-26 15:25:32 -0700

    Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h" 
+/** 
+* prints_half - print half of the string 
+* @str: the input
+* Return: void 
+*/
+void puts_half(char *str)
+{
+int i;
+int j;
+int length;
+
+length = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+length++;
+}
+
+j = (length / 2);
+
+if ((length % 2) == 1)
+{
+j = ((length + 1) / 2);
+}
+
+for (i = j; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
