771a4634f2967a074bde3157975ec3f42e667c50
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-26 13:41:24 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap 2 numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: 0 (success)
+ */
+void swap_int(int *a, int *b)
+{
+int *c = *a;
+*a = *b;
+*b = *c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 #include <unistd.h>
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
