152c21a61813b9b4ebaf1ef4af5b052d22cf9c2b
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-26 13:33:40 -0700

    Reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * reset_to_98 - reset given integer to 98
- * @a: number to be reset
+ * @n: number to be reset
  * Return: 0 (success)
  */
-void reset_to_98(int a)
+void reset_to_98(int *n)
 {
-*a = 98;
+*n = 98;
 }
