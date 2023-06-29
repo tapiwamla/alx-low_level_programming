8079e4297be610b296ab10642ac74385454fe506
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-29 14:48:08 -0700

    Betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 char *cap_string(char *str)
 {
 int i = 0;
-int j = 0;
+int j;
 char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(', ')', '{', '}'};
 
 while (str[i] != '\0')
@@ -22,6 +22,7 @@ str[i] = str[i] - 'a' + 'A';
 }
 else
 {
+j = 0;
 while (j <= 12)
 {
 if (separators[j] == str[i - 1])
