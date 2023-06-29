420ebd83e138bf85ee4c43c7b62e9d54f88b103f
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-29 14:34:13 -0700

    toCap()

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,21 +3,20 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: string
+ *
  * Return: pointer to the modified string
  */
-
 char *cap_string(char *str)
 {
 int i = 0;
 int j;
-char s[] = {' ', '\t', '\n',
-',', ';', '.', '!', '?', '\"', '(', ')', '{', '}'};
+char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(', ')', '{', '}'};
 
 while (str[i] != '\0')
-{        
+{
 if (str[i] >= 'a' && str[i] <= 'z')
 {
-    if (i == 0)
+if (i == 0)
 {
 str[i] = str[i] - 'a' + 'A';
 }
@@ -27,7 +26,7 @@ j = 0;
 
 while (j <= 12)
 {
-if (s[j] == str[i - 1])
+if (separators[j] == str[i - 1])
 {
 str[i] = str[i] - 'a' + 'A';
 break;
