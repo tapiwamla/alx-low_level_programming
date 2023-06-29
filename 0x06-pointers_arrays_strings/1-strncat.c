5a2a5910aa29f270f0fc147151282258463d28a7
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-29 12:36:49 -0700

    Again

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,29 +6,28 @@
  * @src: Pointer to the source string
  * @n: Maximum number of bytes to be taken from src
  *
- * Description: Concatenates at most n bytes from src to the end of dest.
+ * Description:
+ * Concatenates at most n bytes from src to the end of dest.
  * The src string does not need to be null-terminated if it contains n or more bytes.
  *
  * Return: Pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int length = 0;
-	int i;
+    int length = 0;
+    int i;
 
-	while (dest[length] != '\0')
-	{
-		length++;
-	}
+    while (dest[length] != '\0')
+    {
+        length++;
+    }
 
-	i = 0;
-	
-	for (; i < n && src[i] != '\0'; i++)
-	{
-		dest[length] = src[i];
-		length++;
-	}
-	dest[length] = '\0';
-	
-	return (dest);
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[length] = src[i];
+        length++;
+    }
+    dest[length] = '\0';
+
+    return dest;
 }
