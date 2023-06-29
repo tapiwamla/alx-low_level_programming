aa48e3c439d61bc6d100ad813b587405be63ad99
tapiwamla <tmlambo@usiu.ac.ke>
2023-06-29 13:06:54 -0700

    Remove the spaces

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,22 +14,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int length = 0;
-    int i;
+int length = 0;
+int i;
 
-    while (dest[length] != '\0')
-    {
-        length++;
-    }
+while (dest[length] != '\0')
+{
+length++;
+}
 
-    i = 0;
-    
-    for (; i < n && src[i] != '\0'; i++)
-    {
-        dest[length] = src[i];
-        length++;
-    }
-    dest[length] = '\0';
+i = 0;
 
-    return dest;
+for (; i < n && src[i] != '\0'; i++)
+{
+dest[length] = src[i];
+length++;
+}
+dest[length] = '\0';
+
+return dest;
 }
