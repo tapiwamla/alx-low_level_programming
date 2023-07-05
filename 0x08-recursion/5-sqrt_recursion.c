deb14a4f07653b8f9eb155d26ea5133e170139ab
tapiwamla <tmlambo@usiu.ac.ke>
2023-07-05 15:07:57 -0700

    Square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root
+ * @n: the number
+ * Return: square root
+ */
+ 
+int _sqrt_recursion(int n)
+{
+int find_sqrt(int i, int j);
+return (find_sqrt(1, n));
+}
+
+/**
+ * find_sqrt - recursion function to find square root
+ * @i: number to square
+ * @j: square of i
+ * Return: number
+ */
+
+int find_sqrt(int i, int j)
+{
+
+if (i * i > j)
+return (-1);
+
+if (i * i == j)
+return (i);
+
+else
+return (find_sqrt((i + 1), j));
+}
