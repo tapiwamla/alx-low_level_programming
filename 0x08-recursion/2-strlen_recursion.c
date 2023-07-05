609451e2512318dfc0ba4f6320bdc9b40fad7fbb
tapiwamla <tmlambo@usiu.ac.ke>
2023-07-05 12:42:48 -0700

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print length of string
+ * @s: string pointer
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+return (0);
+}
+
+return (1 + _strlen_recursion(s + 1));
+}
