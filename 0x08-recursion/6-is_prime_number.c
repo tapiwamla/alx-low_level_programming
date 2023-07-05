a1ab3f6b53cdba23e9f53ffd080a22b1081d61bb
tapiwamla <tmlambo@usiu.ac.ke>
2023-07-05 14:37:10 -0700

    Prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *is_prime_number - returns 1 for prime, 0 otherwise
+ *@n: integer
+ *Return: number
+ */
+
+int is_prime_number(int n)
+{
+int i = 1;
+
+return (prime_calc(i, n));
+}
+
+/**
+ *prime_calc - returns prime numbers
+ *@i: count variable
+ *@j: int
+ *Return: int
+ */
+ 
+int prime_calc(int i, int j)
+{
+
+if (j == 1 || j == 25)
+return (0);
+
+else if (j <= 0)
+return (0);
+
+else if (j % 2 == 0)
+return (0);
+
+else if (j % 2 != 0)
+return (1);
+
+return (prime_calc(i * (i + 1), j));
+}
