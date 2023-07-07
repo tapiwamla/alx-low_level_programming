f0c2520a92f9ca256dcfda799920ec4cff9c4d5a
tapiwamla <tmlambo@usiu.ac.ke>
2023-07-07 13:09:18 -0700

    Oh nah nah

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+ 
+ int main(int argc, char **argv)
+ {
+ printf("%s\n", argv[0]);
+ return (0);
+ }
