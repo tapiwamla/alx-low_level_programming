dadf9aec26284982534857c8ac13eec049a58e1a
tapiwamla <tmlambo@usiu.ac.ke>
2023-07-11 10:07:38 -0700

    Grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - pointer to 2D array
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2D array
+ */
+ 
+int **alloc_grid(int width, int height)
+{
+int **myarr;
+int i;
+int j;
+
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+
+myarr = malloc(sizeof(int *) * height);
+
+if (myarr == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < height; i++)
+{
+myarr[i] = malloc(sizeof(int) * width);
+
+if (myarr[i] == NULL)
+{
+for (; i >= 0; i--)
+free(myarr[i]);
+free(myarr);
+return (NULL);
+}
+}
+
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+mjarr[i][j] = 0;
+}
+
+return (myarr);
+}
