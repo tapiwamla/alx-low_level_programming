46c0e424f83a29feb22e35fc17c57c5fa576ef22
tapiwamla <tmlambo@usiu.ac.ke>
2023-07-20 03:17:27 -0700

    Variable, not static

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print numbers, followed by new line.
+ * @separator: number separator (string).
+ * @n: integers (n).
+ * @...: variable size.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list strings;
+char *str;
+unsigned int x;
+
+
+va_start(strings, n);
+
+
+for (x = 0; x < n; x++)
+{
+str = va_arg(strings, char *);
+
+
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+
+if (x != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+
+printf("\n");
+
+
+va_end(strings);
+}
+
