6ce5bcf8b90dc55c16a7b2473e49bce18a24e46c
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-08-31 07:16:45 -0700

    Big or little endian.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+unsigned int index;
+char *character;
+
+/* Set index to 1 to check the first byte. */
+index = 1;
+
+/* Set character to index. */
+*character = (char *) &index;
+
+return (*character);
+}
+
