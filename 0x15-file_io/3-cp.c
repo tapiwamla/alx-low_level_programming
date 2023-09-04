4a26f1eb7442655af1dddd053a27abe242ee9059
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-09-04 10:15:19 -0700

    Requirement fail: last try.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,8 @@ this_buffer = malloc(sizeof(char) * 1024);
 /* If malloc fails, error and exit with NULL. */
 if (this_buffer == NULL)
 {
-_dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+_dprintf(STDERR_FILENO, 
+"Error: Can't write to %s\n", filename);
 
 exit(99);
 }
@@ -41,7 +42,8 @@ custom_close = close(fd);
 /* If closing the file fails, error and exit with 100. */
 if (custom_close == -1)
 {
-_dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+_dprintf(STDERR_FILENO, 
+"Error: Can't close fd %d\n", fd);
 
 exit(100);
 }
@@ -63,7 +65,8 @@ char *buffer;
 /* If the number of arguments is incorrect, error and exit with 97 (usage). */
 if (argc != 3)
 {
-_dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+_dprintf(STDERR_FILENO, 
+"Usage: cp file_from file_to\n");
 
 exit(97);
 }
@@ -84,7 +87,8 @@ file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
 if (file_from == -1 || file_read == -1)
 {
-_dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+_dprintf(STDERR_FILENO, 
+"Error: Can't read from file %s\n", argv[1]);
 
 free(buffer);
 
@@ -96,7 +100,8 @@ file_write = write(file_to, buffer, file_read);
 /* If writing fails, error and exit with 99 (cannot write to). */
 if (file_to == -1 || file_write == -1)
 {
-_dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+_dprintf(STDERR_FILENO, 
+"Error: Can't write to %s\n", argv[2]);
 
 free(buffer);
 
