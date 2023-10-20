a6928de7204662260994ff6f7e2f75babe4c76c7
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-10-20 08:09:13 -0700

    Try with empty functions.

---
+++ 0x18-dynamic_libraries/libfunctions.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+int _putchar(char c) {
+    /* This is an empty function */
+    return 0;
+}
+
+int _islower(int c) {
+    /* This is an empty function */
+    return 0;
+}
+
+int _isalpha(int c) {
+    /* This is an empty function */
+    return 0;
+}
+
+int _abs(int n) {
+    /* This is an empty function */
+    return 0;
+}
+
+int _isupper(int c) {
+    /* This is an empty function */
+    return 0;
+}
+
+int _isdigit(int c) {
+    /* This is an empty function */
+    return 0;
+}
+
+int _strlen(char *s) {
+    /* This is an empty function */
+    return 0;
+}
+
+void _puts(char *s) {
+    /* This is an empty function */
+}
+
+char *_strcpy(char *dest, char *src) {
+    /* This is an empty function */
+    return dest;
+}
+
+int _atoi(char *s) {
+    /* This is an empty function */
+    return 0;
+}
+
+char *_strcat(char *dest, char *src) {
+    /* This is an empty function */
+    return dest;
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    /* This is an empty function */
+    return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    /* This is an empty function */
+    return dest;
+}
+
+int _strcmp(char *s1, char *s2) {
+    /* This is an empty function */
+    return 0;
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    /* This is an empty function */
+    return s;
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    /* This is an empty function */
+    return dest;
+}
+
+char *_strchr(char *s, char c) {
+    /* This is an empty function */
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    /* This is an empty function */
+    return 0;
+}
+
+char *_strpbrk(char *s, char *accept) {
+    /* This is an empty function */
+}
+
+char *_strstr(char *haystack, char *needle) {
+    /* This is an empty function */
+}
+
