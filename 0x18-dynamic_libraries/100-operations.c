44e33d212821f0145f66e95e464a68dccba5f54b
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2023-10-20 09:25:14 -0700

    All operations.

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,9 +1,34 @@
-/* Function to add two numbers */
+#include <stdio.h>
+
 int add(int a, int b) {
-	return a + b;
+    return a + b;
 }
 
-/* Function to multiply two numbers */
-int multiply(int a, int b) {
-	return a * b;
+int sub(int a, int b) {
+    return a - b;
 }
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error: Division by zero\n");
+        return 0;
+    }
+    return a / b;
+}
+
+int mod(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error: Division by zero\n");
+        return 0;
+    }
+    return a % b;
+}
+
